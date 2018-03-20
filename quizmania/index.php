ffdf4ca28f69c26776ed3c53ea9d46b11530d833
imramk98 <37440063+imramk98@users.noreply.github.com>
2018-03-20 13:12:36 +0000

    questions display

--- quizmania/index.php
+++ quizmania/index.php
@@ -11,8 +11,20 @@
 					//echo 'You are logged in '.$resultCheck['username'];
 					echo '<section class="main-container">';
 					echo '<div class="main-wrapper">';
-					echo '<h2>Welcome '.$resultCheck['username'];
-					echo '</h2';
+					echo '<h2>Welcome '.$resultCheck['firstName'];
+					echo '</h2>';
+					echo '</br>';
+					echo '<h2>Put your knowledge to test by taking the following multiple choice questions</h2>';	
+					echo '</br>';
+					echo '<div class="unorderlist">';				
+					echo '<ul>';
+					echo '<li><Strong>Catagorie: </strong>General knowledge</li>';
+					echo '<li><Strong>Number Of questions: </strong>10</li>';
+					echo '<li><Strong>Estimated time for completion: </strong>10 Minutes</li>';
+					echo '</br>';
+					echo '<a href="questions.php?n=1" class="startquiz">Start Quiz</a>';
+					echo '</ul>';
+					echo '</div>';
 					echo '</div>';
 					echo '</section>';
 				}else{
